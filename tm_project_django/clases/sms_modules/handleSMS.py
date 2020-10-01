23ff9c1b6df0d1ecc1e77716d6f64b65e6528733
mamzin-server <yoko11.06.92@yandex.ru>
2020-10-01 08:35:50 +0300

    fix bag in handleSMS.py

--- tm_project_django/clases/sms_modules/handleSMS.py
+++ tm_project_django/clases/sms_modules/handleSMS.py
@@ -4,34 +4,38 @@ import logging
 from gsmmodem.modem import GsmModem
 from tm_project_django.clases.sms_modules.SMS_creator import SMS_creator, save_SMS_in_db
 
-
 message = SMS_creator()
 
-def handleSms(sms):
 
+def handleSms(sms):
     try:
         if sms.udh:
             message.set_SMS(sms.number, sms.text, sms.udh[0].data)
 
-        for i in sms.udh:
-            #print("id = ", i.id)
-            #print("dataLength = ", i.dataLength)
-            print("data = ", i.data)
+            for i in sms.udh:
+                # print("id = ", i.id)
+                # print("dataLength = ", i.dataLength)
+                print("data = ", i.data)
             message.save_SMS_fragments_in_db(sms.udh[0].data)
         else:
-            print('============= Принято короткое СМС =============\nНомер: {0}\nВремя: {1}\nСМС: {2}'.format(sms.number, sms.time, sms.text))
+            print(
+                '============= Принято короткое СМС =============\nНомер: {0}\nВремя: {1}\nСМС: {2}'.format(sms.number,
+                                                                                                            sms.time,
+                                                                                                            sms.text))
             save_SMS_in_db(sms.number, sms.text)
-            print('='*50)
+            print('=' * 50)
 
     except AttributeError:
-        print(f"reference {sms.reference} timeSent {sms.timeSent} timeFinalized {sms.timeFinalized} deliveryStatus {sms.deliveryStatus}")
+        print(
+            f"reference {sms.reference} timeSent {sms.timeSent} timeFinalized {sms.timeFinalized} deliveryStatus {sms.deliveryStatus}")
         return
+
 
 class HandleSMS():
 
-    def __init__(self, PORT = '/dev/ttyUSB0', SPEED = 115200, PIN = None):
+    def __init__(self, PORT='/dev/ttyUSB0', SPEED=115200, PIN=None):
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
-        self.modem = GsmModem(PORT, SPEED, smsReceivedCallbackFunc = handleSms)
+        self.modem = GsmModem(PORT, SPEED, smsReceivedCallbackFunc=handleSms)
         self.modem.smsTextMode = False
         self.modem.connect(PIN)
         print('Initializing modem...')
