198fd4aed5ce8f8df1738a4c33fb7dde83b44354
svyatoslav <yoko11.06.92@yandex.ru>
2020-12-08 09:15:59 +0400

    fix contacts

--- support_contacts/models.py
+++ support_contacts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class Support_contacts (models.Model):
+
+class SupportСontacts (models.Model):
     position = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
--- support_contacts/views.py
+++ support_contacts/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from support_contacts.models import Support_contacts
+from support_contacts.models import SupportСontacts
 
 
 @login_required
 def contacts(request):
-    contacts = Support_contacts.objects.all()
+    contacts = SupportСontacts.objects.all()
 
     data = {"contacts": contacts}
 
-    return render(request, "contacts.html",context=data)
+    return render(request, "contacts.html", context=data)
